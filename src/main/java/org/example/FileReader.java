2f5e28dfc92538736d599e8d26eb4f39be4d7608
Dorian Gillioz <gilliozdorian@gmail.com>
2022-12-01 14:43:56 +0100

    FileReader

---
+++ src/main/java/org/example/FileReader.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.mail.Group;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Classe qui permet de lire le contenu de fichiers pour préparer le prank
+ */
+public class FileReader {
+    private static final Logger LOG = Logger.getLogger(FileReader.class.getName());
+
+
+}
