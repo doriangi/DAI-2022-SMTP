acbd3aade67970bf187fe76e96de4b4f398e455d
Oscar Baume <94351326+obaume@users.noreply.github.com>
2022-12-04 12:25:37 +0100

    ajout file reader et maj du main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,28 @@ package org.example;
 import org.example.mail.Mail;
 import org.example.smtp.SmtpClient;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        String mailFrom = "gilliozdorian@gmail.com";
-        String[] rcptTo = { "dorian.gillioz@heig-vd.ch", "super@mail.com" };
-        String from = "doriancool@gmail.com";
-        String[] to = { "gilliozdorian@gmail.com", "personne@mail.com" };
+        FileReader fr = new FileReader();
+        File victims = new File("C:\\Users\\gilli\\OneDrive\\Documents\\DAI\\DAI-2022-SMTP\\config\\victims\\victims.txt");
+        String[] group = fr.readVictims(victims, 4).toArray(new String[0]);
+
+        String from = group[0];
+        String[] to = new String[group.length - 1];
+
+        System.arraycopy(group, 1, to, 0, group.length - 1);
+
         String subject = "Bonjour";
         String content = "Bonjour, voici mon mail\nbonne journée";
 
-        Mail mail = new Mail(mailFrom, rcptTo, from, to, subject, content);
-        SmtpClient client = new SmtpClient("localhost", 25);
+        Mail mail = new Mail(from, group, from, to, subject, content);
+
+        File configs = new File("C:\\Users\\gilli\\OneDrive\\Documents\\DAI\\DAI-2022-SMTP\\config\\options\\params.txt");
+        String address = fr.readAddress(configs);
+        int port = fr.readPort(configs);
+        SmtpClient client = new SmtpClient(address, port);
 
         client.sendMail(mail);
     }
