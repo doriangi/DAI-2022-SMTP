2f0bab3c240d443f0b5363d4b31e73901c77c2d5
Oscar Baume <94351326+obaume@users.noreply.github.com>
2022-12-04 14:54:48 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,73 @@
 package org.example;
 
 import org.example.mail.Mail;
+import org.example.prank.PrankManager;
 import org.example.smtp.SmtpClient;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        FileReader fr = new FileReader();
-        File victims = new File("C:\\Users\\gilli\\OneDrive\\Documents\\DAI\\DAI-2022-SMTP\\config\\victims\\victims.txt");
-        String[] group = fr.readVictims(victims, 4).toArray(new String[0]);
+        BufferedReader stdin = null;
 
-        String from = group[0];
-        String[] to = new String[group.length - 1];
+        try {
+            stdin = new BufferedReader(new InputStreamReader(System.in));
 
-        System.arraycopy(group, 1, to, 0, group.length - 1);
+            System.out.println("Veuillez entrer le chemin complet du fichier contenant les victimes :");
+            String victim_path = stdin.readLine();
 
-        String subject = "Bonjour";
-        String content = "Bonjour, voici mon mail\nbonne journée";
+            System.out.println("Veuillez entrer le chemin complet du fichier contenant la liste des messages :");
+            String msg_path = stdin.readLine();
 
-        Mail mail = new Mail(from, group, from, to, subject, content);
+            System.out.println("Combien de groupes voulez-vous créer ?");
+            int size = Integer.parseInt(stdin.readLine());
+            PrankManager generator = new PrankManager(size);
 
-        File configs = new File("C:\\Users\\gilli\\OneDrive\\Documents\\DAI\\DAI-2022-SMTP\\config\\options\\params.txt");
-        String address = fr.readAddress(configs);
-        int port = fr.readPort(configs);
-        SmtpClient client = new SmtpClient(address, port);
+            int nbr_victims = 0;
+            for (int i = 0; i < size; ++i) {
+                System.out.printf("Combien de victimes voulez-vous dans le groupe #%d ?\n", i + 1);
+                nbr_victims = Integer.parseInt(stdin.readLine());
+                List<String> victims = generator.addGroup(victim_path, nbr_victims);
+                System.out.printf("Groupe #%d créé\n", i + 1);
+                System.out.print("Victimes du groupe :\n");
+                for (String victim : victims) System.out.println(victim);
+            }
 
-        client.sendMail(mail);
+            String choice = "";
+            do {
+                int grp_number = 1;
+                if (size > 1) { // Pas besoin de demander quel groupe quand il n'y a qu'un
+                    System.out.println("Quel groupe voulez-vous piéger ?");
+                    grp_number = Integer.parseInt(stdin.readLine());
+                }
+                Mail prank = generator.setPrankMail(generator.getGroup(grp_number - 1), msg_path);
+
+                String currentPath = new java.io.File(".").getCanonicalPath();
+                String config = currentPath + "\\config\\options\\params.txt";
+                SmtpClient client = generator.connect(config);
+                client.sendMail(prank);
+                System.out.printf("Groupe #%d piégé\n", grp_number);
+
+                System.out.println("Voulez-vous piéger un autre groupe ? [Y/N]");
+                choice = stdin.readLine();
+            } while (choice.equalsIgnoreCase("Y"));
+            stdin.close();
+        } catch (IOException e) {
+            if (stdin != null) {
+                try {
+                    stdin.close();
+                } catch (IOException e1) {
+                    LOG.log(Level.SEVERE, e.getMessage());
+                }
+            }
+            LOG.log(Level.SEVERE, e.getMessage());
+        }
     }
 }
